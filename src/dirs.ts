ee445e37f8bb36bffa9c676fb9f90bd3a65e3c3a
Sylvain <sylvaincau31@gmail.com>
2022-05-28 23:36:37 +0800

    cleanup

--- src/dirs.ts
+++
@@ -1,9 +0,0 @@
-export function home(): string | null {
-  switch (Deno.build.os) {
-    case "linux":
-    case "darwin":
-      return Deno.env.get("HOME") ?? null;
-    case "windows":
-      return Deno.env.get("USERPROFILE") ?? null;
-  }
-}
